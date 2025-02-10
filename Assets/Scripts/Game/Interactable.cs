29498f30ed35248b8429ce4dc1f26e5fde9b0362
Tobynator <noellgerome06@gmail.com>
2025-02-10 17:59:11 +0100

    AnimKrams

--- Assets/Scripts/Game/Interactable.cs
+++ Assets/Scripts/Game/Interactable.cs
@@ -2,10 +2,24 @@ using Assets.Scripts;
 using Unity.Netcode;
 using UnityEngine;
 
-public abstract class Interactable : MonoBehaviour
+public abstract class Interactable : NetworkBehaviour
 {
-    protected bool isInteractable;
+    private bool isSpawned = false;
+    protected bool isInteractable = true;
 
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        isSpawned = true;
+
+        OnSpawned();
+    }
+    protected virtual void OnSpawned()
+    {
+        
+    }
 
     protected virtual void OnInteract(ulong id, AMServerManger serverManger)
     {
@@ -15,6 +29,9 @@ public abstract class Interactable : MonoBehaviour
     [ServerRpc]
     public void Interact_ServerRpc(ulong id)
     {
+        if (isInteractable == false || isSpawned == false)
+            return;
+
         var serverManager = GameObject.FindGameObjectWithTag("ServerManager");
 
         if (serverManager != null)
--- Assets/Scripts/Game/Interactables/DoorPick.cs
+++ Assets/Scripts/Game/Interactables/DoorPick.cs
@@ -1,9 +1,37 @@
+using Unity.Netcode;
 using UnityEngine;
 
+public enum DoorState
+{
+    none, openRight, openLeft, closeRight, closeLeft, 
+}
 public class DoorPickInteractable : Interactable
 {
+    private GameObject DoorFront;
+    private GameObject DoorBack;
+
+    protected Animator animator = null;
+    protected DoorState doorState = DoorState.none;
+
+    protected bool isUnlocked = false;
+    
+    protected override void OnSpawned()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     protected override void OnInteract(ulong id, AMServerManger serverManger)
     {
         
     }
+
+    [ClientRpc]
+    private void OpenDoor_ClientRpc()
+    {
+
+
+
+    }
+
+   
 }
