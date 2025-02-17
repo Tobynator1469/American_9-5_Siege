83fe1d3053cafcc431f245987b09992a6f63b818
Tobynator <noellgerome06@gmail.com>
2025-02-17 13:04:45 +0100

    Itemspawn spawnt jtz was

--- Assets/Scripts/Game/Items/ItemSpawn.cs
+++ Assets/Scripts/Game/Items/ItemSpawn.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -43,8 +44,42 @@ public class ItemSpawn : NetworkBehaviour
 
         Debug.Log($"Der Spawn {listObj.name} hat den Tag {listObj.tag}.");
 
+        // Wähle zufällig eines der drei Arrays
+        GameObject[][] arrays = { Weapons, LowValueStuff, MediumValueStuff };
+        GameObject[] selectedArray = arrays[UnityEngine.Random.Range(0, arrays.Length)];
+
+        if (selectedArray.Length == 0)
+        {
+            Debug.LogWarning("Das gewählte Array ist leer!");
+            return;
+        }
+
+        GameObject selectedObject = selectedArray[UnityEngine.Random.Range(0, selectedArray.Length)];
+
+        Instantiate(selectedObject, listObj.transform.position, Quaternion.identity);
 
     }
+
+    /*
+     void SpawnRandomObject()
+    {
+        // Wähle zufällig eines der drei Arrays
+        GameObject[][] arrays = { array1, array2, array3 };
+        GameObject[] selectedArray = arrays[Random.Range(0, arrays.Length)];
+        
+        if (selectedArray.Length == 0)
+        {
+            Debug.LogWarning("Das gewählte Array ist leer!");
+            return;
+        }
+        
+        // Wähle zufällig ein GameObject aus dem gewählten Array
+        GameObject selectedObject = selectedArray[Random.Range(0, selectedArray.Length)];
+        
+        // Instanziiere das GameObject an der Position des SPAWNPOINTs
+        Instantiate(selectedObject, SPAWNPOINT.position, Quaternion.identity);
+    }
+     */
 
     public void SpawnItems()
     {
